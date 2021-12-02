7211bb17550e4dd29651d734022ac686c2871f13
djlisko01 <58869028+djlisko01@users.noreply.github.com>
2021-12-02 12:50:19 -0800

    Update uri.js

--- database/uri.js
+++ database/uri.js
@@ -1,3 +1,5 @@
+// From Daniel -> You should not put you MongoDB credentials here. YOu should make a .env file that you keep yourself that won't get passed to github.
+
 const { MongoClient } = require("mongodb");
 const uri =
   "mongodb+srv://gyouza:Jiaozi123@cluster0.ark8o.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
